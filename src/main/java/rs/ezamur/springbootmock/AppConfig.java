1cb71c12e062bf2485fe84469da4fee155933a51
ezamur <ezamur@gmail.com>
2016-02-20 12:35:03 +0100

    Connecting to remote database.

--- src/main/java/rs/ezamur/springbootmock/AppConfig.java
+++ src/main/java/rs/ezamur/springbootmock/AppConfig.java
@@ -1,6 +1,7 @@
 package rs.ezamur.springbootmock;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +11,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 @EnableAutoConfiguration
@@ -18,18 +20,35 @@ import javax.sql.DataSource;
 @ComponentScan
 public class AppConfig {
 
-    @Autowired
-    private DataSource dataSource;
+    org.slf4j.Logger logger = LoggerFactory.getLogger(AppConfig.class);
 
     public static void main(String[] args) {
         SpringApplication.run(AppConfig.class, args);
     }
 
     @Bean
-    EntityManagerFactory entityManagerFactory() {
+    EntityManagerFactory entityManagerFactory() throws URISyntaxException {
         LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
-        bean.setDataSource(dataSource);
+        bean.setDataSource(dataSource());
 
         return bean.getObject();
+    }
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        // Putting this value in here since DATABASE_URL returns null although it is set.
+        String envValue = "postgres://ksnhojohzxydmr:4wzuTDNTYwBKRVZBOR-zqPRf9G@ec2-54-83-198-111.compute-1.amazonaws.com:5432/d9nireb0hc08c2";
+        URI dbUri = new URI(envValue);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?ssl=true";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
 }
